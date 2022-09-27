b197f410328c4f4603e496b1ddf704506126566b
kennysliding <kennychiu.v@gmail.com>
2022-09-27 13:33:53 +0800

    configs for hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,35 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-tracer";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.13",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    // mainnet: {},
+    goerli: {
+      url: process.env.GOERLI_URL as string,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+    hardhat: {},
+    ganache: {
+      url: "http://127.0.0.1:7545",
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
